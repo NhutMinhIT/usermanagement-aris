1accc82d3148caea75ceaa9e98b8cbfd87f45871
NhutMnhIT <nhutminh.it19@gmail.com>
2024-12-23 16:37:18 +0700

    fix fixx

--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -1,21 +1,22 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { JwtModule } from '@nestjs/jwt'; // Import JwtModule
+import { JwtModule } from '@nestjs/jwt';
 import { UserSchema } from "../auth/entities/user.entity";
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { AuthModule } from '../auth/auth.module'; // Import AuthModule
+import { AuthModule } from '../auth/auth.module';
+import { JwtStrategy } from '../auth/strategies/jwt.strategy'; // Import JwtStrategy
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
         AuthModule,
         JwtModule.register({
-            secret: process.env.JWT_SECRET, // Replace with your secret key
+            secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '3h' },
         }),
     ],
     controllers: [UserController],
-    providers: [UserService]
+    providers: [UserService, JwtStrategy] // Add JwtStrategy to providers
 })
 export class UserModule { }
