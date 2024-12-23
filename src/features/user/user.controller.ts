62320af06ad3793f10df0a2c89b2163189cbca78
NhutMnhIT <nhutminh.it19@gmail.com>
2024-12-23 16:19:12 +0700

    refactor: change route of get all user

--- src/features/user/user.controller.ts
+++ src/features/user/user.controller.ts
@@ -4,7 +4,7 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Roles } from '../auth/decorators/role.decorator';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
