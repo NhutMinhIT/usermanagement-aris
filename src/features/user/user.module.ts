1f6fdf404b5a10b35993dfcd11663ebce9ec645d
NhutMnhIT <nhutminh.it19@gmail.com>
2024-12-23 16:28:00 +0700

    fix: missing user module

--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { UserSchema } from "../auth/entities/user.entity";
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { AuthModule } from '../auth/auth.module'; // Import AuthModule
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+        AuthModule
+    ],
     controllers: [UserController],
     providers: [UserService]
 })
