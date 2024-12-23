e9333d390bad0bdd1a060839ebe11d5d8e3c65b3
NhutMnhIT <nhutminh.it19@gmail.com>
2024-12-23 16:34:29 +0700

    fix: user module

--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { JwtModule } from '@nestjs/jwt'; // Import JwtModule
 import { UserSchema } from "../auth/entities/user.entity";
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
@@ -8,7 +9,11 @@ import { AuthModule } from '../auth/auth.module'; // Import AuthModule
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-        AuthModule
+        AuthModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET, // Replace with your secret key
+            signOptions: { expiresIn: '3h' },
+        }),
     ],
     controllers: [UserController],
     providers: [UserService]
